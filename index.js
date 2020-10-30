d2cb266c8ac6c3476d1991dd1b3a627adbbacd66
Paul van Mierlo <pauljozefvanmierlo@yahoo.com>
2020-10-30 17:27:33 +0100

    nieuwe file

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+function huiswerkMaken(vak, callback) {
+    setTimeout(() => {
+        console.log(`ok,ok ik ga nu mijn ${vak} huiswerk maken`);
+        setTimeout(() => callback(vak), 6000);
+    }, 5000);
+};
 
+function klaarMetHuiswerk(vak2) { console.log(`Kijk paps, mams, ik ben klaar met mijn ${vak2} huiswerk`); }
+
+function huiswerkIntentie(vak3) {
+    huiswerkMaken(vak3, klaarMetHuiswerk)
+    console.log(`Nee ik heb geen zin om ${vak3} huiswerk te maken. Ga maar even wachten ......`);
+}
+
+huiswerkIntentie('wiskunde');
+huiswerkIntentie('Frans');
+huiswerkIntentie('Spaans');
